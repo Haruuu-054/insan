aacc782a1bafef5a0bcd5d72e745c5be81ecf452
Haruuu-054 <138289636+Haruuu-054@users.noreply.github.com>
2024-11-19 10:47:29 +0800

    Add files via upload

---
+++ infinity.js
@@ -0,0 +1,42 @@
+// console.log(fetch('https://www.shutterstock.com/image-photo/beautiful-wonderful-cute-kitten-wallpaper-600nw-2435308283.jpg')
+// .then(response=>{
+//     // console.log(response)
+//     return response.blob();
+// })
+// .then(blob=>{
+//     // console.log(blob)
+//     document.querySelector('#cute-cat').src=URL.createObjectURL(blob)
+// })
+// .catch(error=>{
+//     console.log(error)
+// })
+
+// )
+
+//id selector
+const content = document.querySelector("#content")
+//loading page
+window.addEventListener("load", ()=>{
+    getUser();
+})
+
+function getUser(){
+    //define html variable
+    let html=""
+     fetch("https://bscs3a-api-crud-1-h9m9.onrender.com/api/members", {mode: "cors"})
+    // fetch("http://localhost:5000/api/members", {mode: "cors"})
+    .then(response=>{
+        console.log(response)
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        data.forEach((element)=>{
+            html += `<li>${element.first_name} ${element.last_name}</li>`
+        })
+        content.innerHTML = html
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
